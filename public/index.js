1e3ce1cc5ef12a7e16354dd8c780312eaad9be3f
Evans <ellaboevans@gmail.com>
2022-12-05 00:10:35 +0000

    Add script file

---
+++ public/index.js
@@ -0,0 +1,7 @@
+const menu = document.querySelector("#menu");
+const burger = document.querySelector("#burger");
+
+
+burger.addEventListener("click", () =>{
+    menu.classList.toggle("hidden")
+})
