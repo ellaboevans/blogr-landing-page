b11d155e1e1e3c0e3b9ab030f2f150b40c8c8e30
Evans <ellaboevans@gmail.com>
2022-12-05 21:45:04 +0000

    Add copyright year dynamically

--- public/index.js
+++ public/index.js
@@ -1,7 +1,25 @@
+// Menu Display
+
 const menu = document.querySelector("#menu");
 const burger = document.querySelector("#burger");
 
+burger.addEventListener("click", () => {
+  menu.classList.toggle("hidden");
+});
 
-burger.addEventListener("click", () =>{
-    menu.classList.toggle("hidden")
-})
+// Copyright Auto Update
+
+const copyright = document.querySelector("#copyrightDate");
+
+
+
+let update = () => {
+  let date = new Date();
+  //   console.log(date);
+
+  let year = date.getFullYear();
+  //   console.log(year)
+
+  copyright.innerHTML = year;
+};
+update()
