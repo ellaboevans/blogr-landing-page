8076ab987a1025e9eece3acd4b9d595492550b5a
Evans <ellaboevans@gmail.com>
2022-12-06 22:35:55 +0000

    Modify dropdown event

--- public/index.js
+++ public/index.js
@@ -28,22 +28,34 @@ update();
 const dropBtn = document.querySelector("#dropbtn");
 const dropdownContent = document.querySelector("#dropdown-content");
 
+dropBtn.addEventListener("mouseover", function () {
+  dropdownContent.classList.remove("hidden");
+});
+
 dropBtn.addEventListener("click", function () {
-  dropdownContent.classList.toggle("hidden");
+  dropdownContent.classList.add("hidden");
 });
 
 // Company Dropdown
 const dropBtn2 = document.querySelector("#dropbtn-2");
 const dropdownContent2 = document.querySelector("#dropdown-content-2");
 
+dropBtn2.addEventListener("mouseover", function () {
+  dropdownContent2.classList.remove("hidden");
+});
+
 dropBtn2.addEventListener("click", function () {
-  dropdownContent2.classList.toggle("hidden");
+  dropdownContent2.classList.add("hidden");
 });
 
 // Connect Dropdown
 const dropBtn3 = document.querySelector("#dropbtn-3");
 const dropdownContent3 = document.querySelector("#dropdown-content-3");
 
+dropBtn3.addEventListener("mouseover", function () {
+  dropdownContent3.classList.remove("hidden");
+});
+
 dropBtn3.addEventListener("click", function () {
-  dropdownContent3.classList.toggle("hidden");
+  dropdownContent3.classList.add("hidden");
 });
