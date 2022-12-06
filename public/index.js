5604d2e5ae9be5b09833060397bbe75cf9d63faf
Evans <ellaboevans@gmail.com>
2022-12-06 17:53:30 +0000

    Add click even to dropdown link

--- public/index.js
+++ public/index.js
@@ -11,8 +11,6 @@ burger.addEventListener("click", () => {
 
 const copyright = document.querySelector("#copyrightDate");
 
-
-
 let update = () => {
   let date = new Date();
   //   console.log(date);
@@ -22,4 +20,30 @@ let update = () => {
 
   copyright.innerHTML = year;
 };
-update()
+update();
+
+// Dropdown Settings Here
+
+// Product Dropdown
+const dropBtn = document.querySelector("#dropbtn");
+const dropdownContent = document.querySelector("#dropdown-content");
+
+dropBtn.addEventListener("click", function () {
+  dropdownContent.classList.toggle("hidden");
+});
+
+// Company Dropdown
+const dropBtn2 = document.querySelector("#dropbtn-2");
+const dropdownContent2 = document.querySelector("#dropdown-content-2");
+
+dropBtn2.addEventListener("click", function () {
+  dropdownContent2.classList.toggle("hidden");
+});
+
+// Connect Dropdown
+const dropBtn3 = document.querySelector("#dropbtn-3");
+const dropdownContent3 = document.querySelector("#dropdown-content-3");
+
+dropBtn3.addEventListener("click", function () {
+  dropdownContent3.classList.toggle("hidden");
+});
